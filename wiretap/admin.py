332459c20d09ae6b464aca9bb195a3c1cc012ca0
Karthic Raghupathi <karthicr@gmail.com>
2026-05-09 12:45:11 -0400

    style: apply ruff format to admin.py

--- wiretap/admin.py
+++ wiretap/admin.py
@@ -36,14 +36,10 @@ class MessageAdmin(admin.ModelAdmin):
     def has_add_permission(self, request: HttpRequest) -> bool:
         return False
 
-    def has_change_permission(
-        self, request: HttpRequest, obj: Any = None
-    ) -> bool:
+    def has_change_permission(self, request: HttpRequest, obj: Any = None) -> bool:
         return False
 
-    def has_delete_permission(
-        self, request: HttpRequest, obj: Any = None
-    ) -> bool:
+    def has_delete_permission(self, request: HttpRequest, obj: Any = None) -> bool:
         return False
 
 
